8d7ffe1718c5486d5b233e97452fdbb8bcaca897
Kenn Vun Gan <kennvun@hotmail.com>
2022-03-03 21:43:22 +0900

    Add base url for api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const apiKey = process.env.API_KEY;
+const baseUrl = `https://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
 
