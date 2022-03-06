c08745cd4c394db9b16e661af41035dc7712f49d
Kenn Vun Gan <kennvun@hotmail.com>
2022-03-07 00:44:56 +0900

    Replace amazon.co.jp with .com

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/products/:productId", async (req, res) => {
   const { api_key } = req.query;
 
   try {
-    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.co.jp/dp/${productId}`);
+    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`);
 
     res.json(JSON.parse(response));
   } catch (error) {
@@ -31,7 +31,7 @@ app.get("/products/:productId/reviews", async (req, res) => {
   const { api_key } = req.query;
 
   try {
-    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.co.jp/product-review/${productId}`);
+    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/product-review/${productId}`);
 
     res.json(JSON.parse(response));
   } catch (error) {
@@ -45,7 +45,7 @@ app.get("/products/:productId/offers", async (req, res) => {
   const { api_key } = req.query;
 
   try {
-    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.co.jp/gp/offer-listing/${productId}`);
+    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
 
     res.json(JSON.parse(response));
   } catch (error) {
@@ -59,7 +59,7 @@ app.get("/search/:searchQuery", async (req, res) => {
   const { api_key } = req.query;
 
   try {
-    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.co.jp/s?k=${searchQuery}`);
+    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`);
 
     res.json(JSON.parse(response));
   } catch (error) {
